51f9a3220e31e50d4ec15c25a7920215960e7cea
Auston <auston.salvana@live.com>
2015-07-03 13:20:52 -0700

    Initial Commit

---
+++ PigLatin/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PigLatin
+//
+//  Created by Auston Salvana on 7/3/15.
+//  Copyright (c) 2015 ASolo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
